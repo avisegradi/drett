d9fb83cd4e65883d8fc8b5d16818809c18b7a185
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-08-26 09:25:47 +0200

    Fixup dependencies

--- client/setup.py
+++ client/setup.py
@@ -3,19 +3,28 @@
 import setuptools
 from pip.req import parse_requirements
 
-reqs = [ str(i.req) for i in parse_requirements('requirements.txt') ]
-
 setuptools.setup(
     name='drett-client',
     version='0.1.0',
     author='Adam Visegradi',
     author_email='a.visegradi@gmail.com',
-    namespace_packages=['drett'],
-    packages=['drett.client'],
-    scripts=['drett-client-test'],
+    namespace_packages=[
+        'drett',
+    ],
+    packages=[
+        'drett.client',
+    ],
+    scripts=[
+        'drett-client-test',
+    ],
     url='https://github.com/avisegradi/drett',
     license='LICENSE.txt',
     description='Distributed Resource Tracking',
     long_description=open('README.txt').read(),
-    install_requires=reqs,
+    install_requires=[
+        'argparse',
+        'pymongo',
+        'PyYAML',
+        'requests',
+    ],
 )
